dd687e80bf943c87b46c8c2dfed6aac7ca4cf47e
HadiOZ <hadioz616@gmail.com>
2022-11-15 12:26:11 +0700

    add activity storage

---
+++ storage/activitiy/repository.go
@@ -0,0 +1,42 @@
+package activitiy
+
+import (
+	"database/sql"
+	"testfiber/storage/entities"
+)
+
+type Repository interface {
+	Create(*entities.Activity) error
+}
+
+type repository struct {
+	conn *sql.DB
+}
+
+func NewRepository(connection *sql.DB) Repository {
+	return &repository{
+		conn: connection,
+	}
+}
+
+func (r *repository) Create(activity *entities.Activity) error {
+	return nil
+}
+
+func (r *repository) Read(where map[string]string) (*entities.Activity, error) {
+	result := entities.Activity{}
+	return &result, nil
+}
+
+func (r *repository) Reads(where map[string]string) (*entities.Activity, error) {
+	result := entities.Activity{}
+	return &result, nil
+}
+
+func (r *repository) Update(activity *entities.Activity) error {
+	return nil
+}
+
+func (r *repository) Delete(where map[string]string) error {
+	return nil
+}
